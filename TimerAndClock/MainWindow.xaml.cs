aa578168d819c61a93fb8d3e308e1fb59e128e79
hitvz <23214540+Hitvz@users.noreply.github.com>
2019-08-25 23:26:04 +0800

    添加计时代码，并计算基准是根据宽还是高，fontsize根据基准值算出

--- TimerAndClock/MainWindow.xaml.cs
+++ TimerAndClock/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace TimerAndClock
 {
@@ -20,6 +21,9 @@ namespace TimerAndClock
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private DispatcherTimer dTimer;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -35,6 +39,48 @@ namespace TimerAndClock
 
             Canvas.SetLeft(timer, timeCanvas.Width/2 - timer.ActualWidth / 2.0 - 7.5);
 
+            Console.WriteLine(timer.ActualHeight);
+
+            Console.WriteLine(timer.FontSize);
+
+            Console.WriteLine("timer.FontSize:" + timer.FontSize + "timer.ActualHeight:" + timer.ActualHeight+ "timer.ActualWidth:"+timer.ActualWidth);
+
+            Console.WriteLine("timer.FontSize:" + timer.FontSize + "timeCanvas.Height:" + timeCanvas.Height + "timeCanvas.Width:" + timeCanvas.Width);
+
+
+            double baseWidth = timeCanvas.Width / 0.97 / 8 * 4;
+
+            Console.WriteLine("timer.FontSize:" + timer.FontSize + "Height--:" + timeCanvas.Height + "Width--:" + baseWidth);
+
+            double baseSize = baseWidth;
+
+            if (timeCanvas.Height> baseWidth)
+            {
+                Console.WriteLine("width");
+            }
+            else
+            {
+                Console.WriteLine("height");
+                baseSize = timeCanvas.Height;
+            }
+            timer.FontSize = baseSize * 3 / 4-6;
+        }
+
+        private void App_Loaded(object sender, RoutedEventArgs e)
+        {
+            //设置定时器
+            dTimer = new DispatcherTimer();
+            dTimer.Interval = new TimeSpan(10000000);   //时间间隔为一秒
+            dTimer.Tick += new EventHandler(timer_Tick);//Tick是超过计时间隔发生的事件——即超过1秒触发事件Timer_Tick
+            dTimer.Start();//计时器开始
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+
+            timer.Text = DateTime.Now.ToShortTimeString() ;
+            //Console.WriteLine(DateTime.Now.ToLongTimeString());
+            
         }
     }
 }
